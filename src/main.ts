e1ae801ae39e14011c3d60a409909d6581213f1d
acorn497 <acorn497@naver.com>
2026-03-23 19:31:11 +0900

    Init: Initial commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    origin: process.env.SECURITY_CORS_ORIGIN?.split(',') ?? '*',
+  });
+
+  await app.listen(process.env.SERVER_PORT ?? 3000);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+bootstrap();
