4a387d774e8bda3395e500ef5e495b3374a9567b
acorn497 <acorn497@naver.com>
2026-04-01 17:19:07 +0900

    Add: Auth API 스텁 구현

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,51 @@
+import { Controller, NotImplementedException, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller({ path: '/api/auth' })
+export class AuthController {
+  constructor (
+    private readonly authService: AuthService,
+  ) { };
+  /* 계정 관련 로직 */
+
+  @Post('login')
+  handleLogin() {
+    throw new NotImplementedException();
+  }
+
+  @Post('email-send')
+  handleSendEmail() {
+    throw new NotImplementedException();
+  }
+
+  @Post('email-verification')
+  handleVerifyEmail() {
+    throw new NotImplementedException();
+  }
+
+  @Post('register')
+  handleRegister() {
+    throw new NotImplementedException();
+  }
+
+  // 쿠키 기반 인증 시스템 사용 할건지 합의 필요
+  @Post('refresh')
+  handleRefresh() {
+    throw new NotImplementedException();
+  }
+
+  /**
+   * 계정 복구
+   */
+  @Post('recover')
+  handleRecoverAccount() {
+    throw new NotImplementedException();
+  }  
+
+  @Post('logout')
+  handleLogout() {
+    throw new NotImplementedException(); 
+  }  
+
+
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { MailService } from 'src/mail/mail.service';
+
+@Injectable()
+export class AuthService {
+  constructor (
+    private readonly mailService: MailService,
+  ) { };
+
+  public async registerEmail() {
+    
+  }
+}
