13a93b09ad29e0290be5bdeedbfb8a776830046d
acorn497 <acorn497@naver.com>
2026-04-07 16:00:14 +0900

    Refactor: 메일 관련 로직 삭제

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+<<<<<<< HEAD
 import { MailModule } from 'src/mail/mail.module';
 
 @Module({
   imports: [MailModule],
+=======
+
+@Module({
+>>>>>>> 009d5dd (Refactor: 메일 관련 로직 삭제)
   controllers: [AuthController],
   providers: [AuthService]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
+<<<<<<< HEAD
 import { MailService } from 'src/mail/mail.service';
+=======
+>>>>>>> 009d5dd (Refactor: 메일 관련 로직 삭제)
 
 @Injectable()
 export class AuthService {
   constructor (
+<<<<<<< HEAD
     private readonly mailService: MailService,
+=======
+>>>>>>> 009d5dd (Refactor: 메일 관련 로직 삭제)
   ) { };
 
   public async registerEmail() {
