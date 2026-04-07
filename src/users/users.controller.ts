2a2ac30e4b421e43bb5ce04d40bf75739c277060
acorn497 <acorn497@naver.com>
2026-04-07 14:00:49 +0900

    Add: LoginDto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,12 +7,18 @@ export class UsersController {
     private readonly usersService: UsersService,
   ) { };
 
-
+  /**
+   * 회원 탈퇴 컨트롤러
+   */
   @Delete('withdraw')
   handleWithdrawAccount() {
     throw new NotImplementedException();
   }
 
+  /**
+   * 회원 탈퇴 확인 컨트롤러
+   * @param code handleWithdrawAccount에 요청했을 때 생성된 UUID
+   */
   @Get('withdraw')
   handleConfirmWithdrawAccount(@Query('confirmationCode') code: string) {
     throw new NotImplementedException();
