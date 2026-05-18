abe954571982a17b918cf0733cde42e740e7c151
acorn497 <acorn497@naver.com>
2026-05-18 10:33:54 +0900

    Feat: 입력된 객체의 key를 camelCase로 변환하는 헬퍼 유틸리티 추가

--- src/utils/convert-camel.ts
+++ src/utils/convert-camel.ts
@@ -1,3 +1,29 @@
 export const toCamelCase = (str: string): string => {
   return str.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
 };
+
+/**
+ * 입력된 데이터가 객체인지 판별하는 함수
+ * @param data 이 값이 일반 JSON 객체인가
+ * @returns 객체라면 true, 아니라면 false
+ */
+const isPlainObject = (data: unknown): data is Record<string, unknown> => {
+  return data !== null && typeof data === 'object' && !Array.isArray(data);
+}
+
+export const convertKeysToCamelCase = <T>(data: unknown): T => {
+  if (Array.isArray(data)) {
+    return data.map(convertKeysToCamelCase) as T;
+  }
+
+  if (isPlainObject(data)) {
+    return Object.fromEntries(
+      Object.entries(data).map(([key, val]) => [
+        toCamelCase(key),
+        convertKeysToCamelCase(val)
+      ]),
+    ) as T;
+  }
+
+  return data as T;
+}
