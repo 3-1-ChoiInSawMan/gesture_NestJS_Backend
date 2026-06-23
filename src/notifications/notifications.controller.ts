9b4abbd8442206fb7260961538980bffecd12805
swimim <rnjstndud6500@naver.com>
2026-06-23 19:23:21 +0900

    fix: 알림 생성 엔드포인트 제거

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,9 +1,8 @@
-import { Body, Controller, Get, Param, ParseEnumPipe, ParseIntPipe, Patch, Post, Sse, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseEnumPipe, ParseIntPipe, Patch, Sse, UseGuards } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { JwtGuard } from 'src/guards/jwt.guard';
 import { GetUser } from 'src/decorators/get-user.decorator';
 import { type JwtPayload } from 'src/common/jwt-payload.interface';
-import { CreateNotificationDto } from './dto/client/request/create-notification.dto';
 import { NotificationType } from './enum/notification-type.enum';
 import { UpdateNotificationSettingDto } from './dto/client/request/update-notification-setting.dto';
 
@@ -12,27 +11,6 @@ export class NotificationsController {
   constructor(
     private readonly notificationsService: NotificationsService
   ) { };
-
-  // 알림 생성
-  /*
-    내부에서만 사용하는 로직으로 변경해야 합니다.
-    엔드포인트 삭제 예정이므로 리팩토링 대상에서 제외합니다.
-  */
-  @UseGuards(JwtGuard)
-  @Post()
-  async handleCreateNotification(
-    @Body() body: CreateNotificationDto,
-    @GetUser() user: JwtPayload
-  ) {
-    const { data, message } = await this.notificationsService.createNotification(body, user.idx);
-
-    return {
-      data: {
-        notification: data
-      },
-      message
-    };
-  }
 
   // 알림 구독
   @UseGuards(JwtGuard)
